ab971703291bfcb5dec4bd29162770450c8126cf
Vaibhav Tiwari <vaibhavtiwarisatna2004@gmail.com>
2022-08-19 23:50:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$('#sec-1').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrow: true,
+    dots:true,
+    autoplay: true,
+    adaptiveHeight: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.ads-div .secc').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrow: true,
+    dots:true,
+    autoplay: true,
+    adaptiveHeight: true,
+    autoplaySpeed: 2000,
+  });
